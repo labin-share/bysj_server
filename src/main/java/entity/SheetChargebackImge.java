6680490874acac225b1f6eea49d5c748e1b05613
LINLA3 <labin.lin@cargosmart.com>
2016-03-28 15:12:24 +0800

    add SheetChargebackImge and SheetEvalImge entity

---
+++ src/main/java/entity/SheetChargebackImge.java
@@ -0,0 +1,47 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SHEET_CHARGEBACK_IMG")
+public class SheetChargebackImge {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "ID", length = 4)
+	private int id;
+	@Column(name = "SHEET_ID")
+	private int sheetId;
+	@Column(name = "img")
+	private String img;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getSheetId() {
+		return sheetId;
+	}
+
+	public void setSheetId(int sheetId) {
+		this.sheetId = sheetId;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+}
